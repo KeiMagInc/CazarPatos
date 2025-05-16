9cfbe2d54f4964bc89c53572d3d9f6a4cc4dde72
Kerlly Vizuete <159056093+KeiMagInc@users.noreply.github.com>
2025-05-16 10:31:02 -0500

    Creacion de una nueva Actividad y funcionalidad del boton Login

---
+++ app/src/main/java/com/kerlly/vizuete/cazarpatos/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.kerlly.vizuete.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Este es el botón que al hacer clic abre MainActivity
+        val btnLogin = findViewById<Button>(R.id.buttonLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
